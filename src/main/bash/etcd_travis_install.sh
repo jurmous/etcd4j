f9ab9311242031a2489d27d3ca4378349aa6097d
lburgazzoli <lburgazzoli@gmail.com>
2015-08-19 08:33:12 +0200

    add support for travis-ci

---
+++ src/main/bash/etcd_travis_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ETCD_URL="https://github.com/coreos/etcd/releases/download"
+ETCD_VER="v2.1.1"
+ETCD_ARC="linux-amd64"
+
+# cleanup
+rm -rf etcd-${ETCD_VER}-${ETCD_ARC}.tar.gz
+rm -rf etcd-${ETCD_VER}-${ETCD_ARC}
+rm -rf etcd
+
+mkdir etcd-dist
+
+# install etcd
+curl -L ${ETCD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-${ETCD_ARC}.tar.gz \
+    | tar xzf - \
+        --directory ./etcd-dist \
+        --strip-components=1
+
+# check
+./etcd-dist/etcd --version
+
---
+++ src/main/bash/etcd_travis_run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./etcd-dist/etcd --name 'etcd4j' --data-dir /tmp/etcd4j &
+
