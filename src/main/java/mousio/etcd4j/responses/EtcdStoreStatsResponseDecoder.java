9df023fdd2d0fff0b9d7ca05b42662e4a349b185
lburgazzoli <lburgazzoli@gmail.com>
2015-10-13 18:40:20 +0200

    remove unused imports

--- src/main/java/mousio/etcd4j/responses/EtcdStoreStatsResponseDecoder.java
+++ src/main/java/mousio/etcd4j/responses/EtcdStoreStatsResponseDecoder.java
@@ -1,15 +1,31 @@
+/*
+ * Copyright (c) 2015, contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package mousio.etcd4j.responses;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufInputStream;
 import io.netty.handler.codec.http.HttpHeaders;
 
 import java.io.IOException;
 
 /**
+ * @author Jurriaan Mous
+ * @author Luca Burgazzoli
+ * 
  * Parses the JSON response for storage stats responses
  */
 public class EtcdStoreStatsResponseDecoder extends AbstractJsonResponseDecoder<EtcdStoreStatsResponse> {
