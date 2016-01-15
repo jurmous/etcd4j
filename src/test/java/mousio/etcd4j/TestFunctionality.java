cbe99026970f5b3dc82be9826057ee8c994005dd
Jurriaan Mous <jurmous@jurmo.us>
2016-01-15 11:07:36 +0100

    Fix in order test so it is compatible with multiple versions of etcd

--- src/test/java/mousio/etcd4j/TestFunctionality.java
+++ src/test/java/mousio/etcd4j/TestFunctionality.java
@@ -217,7 +217,8 @@ public class TestFunctionality {
     r = etcd.post("etcd4j_test/queue", "Job2").ttl(20).send().get();
     assertEquals(r.action, EtcdKeyAction.create);
 
-    r = etcd.get(String.format("etcd4j_test/queue/%020d",r.node.createdIndex)).consistent().send().get();
+    r = etcd.get(r.node.key).consistent().send().get();
+    assertTrue(r.node.key.endsWith(r.node.createdIndex+""));
     assertEquals(r.node.value, "Job2");
 
     r = etcd.get("etcd4j_test/queue").consistent().recursive().sorted().send().get();
