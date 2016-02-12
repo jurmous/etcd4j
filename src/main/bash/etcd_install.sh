d064ddefd8614a36c6f17aba56904662622be862
lburgazzoli <lburgazzoli@gmail.com>
2016-02-12 16:18:21 +0100

    Add installation options to etcd_install.sh

--- src/main/bash/etcd_install.sh
+++ src/main/bash/etcd_install.sh
@@ -1,32 +1,63 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 1 ]; then
+DEFAULT_ETCD_VERSION="2.2.4"
 
-    ETCD_PATH=$1
-    ETCD_URL="https://github.com/coreos/etcd/releases/download"
-    ETCD_VER="v2.2.4"
-    ETCD_ARC="linux-amd64"
+function usage {
+    cat <<-END >&2
+    USAGE: $0 [-p ETCD_PATH] [-v ETCD_VERSION]
+            -p ETCD_PATH     # Etcd installation path
+            -v ETCD_VERSION  # Etcd version, default ${DEFAULT_ETCD_VERSION}
+            -h               # this usage message
+END
+    exit
+}
 
-    # cleanup
-    rm -rf ${ETCD_PATH}/etcd-${ETCD_VER}-${ETCD_ARC}.tar.gz
-    rm -rf ${ETCD_PATH}/etcd-${ETCD_VER}-${ETCD_ARC}
-    rm -rf ${ETCD_PATH}/etcd
-    rm -rf ${ETCD_PATH}/default.etcd
+################################################################################
+#
+################################################################################
 
-    if [ ! -d ${ETCD_PATH} ]; then
-        mkdir ${ETCD_PATH}
-    fi
+while getopts p:v:h opt
+do
+    case $opt in
+    p)  export ETCD_PATH=$OPTARG ;;
+    v)  export ETCD_VERSION=$OPTARG ;;  
+    h|?) usage ;;
+    esac
+done
 
-    # install etcd
-    curl -L ${ETCD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-${ETCD_ARC}.tar.gz \
-        | tar xzf - \
-            --directory ${ETCD_PATH} \
-            --strip-components=1
+shift $(( $OPTIND - 1 ))
 
-    # check
-    ${ETCD_PATH}/etcd --version
-else
-    echo "Installation path is missing"
-    exit -1
+if [[ ! $ETCD_PATH ]]; then 
+    echo "Warning, ETCD_PATH is required"
+    usage
 fi
 
+if [[ ! $ETCD_VERSION ]]; then 
+   ETCD_VERSION=${DEFAULT_ETCD_VERSION}
+fi
+
+################################################################################
+#
+################################################################################
+
+ETCD_URL="https://github.com/coreos/etcd/releases/download"
+ETCD_ARC="linux-amd64"
+
+# cleanup
+rm -rf ${ETCD_PATH}/etcd-v${ETCD_VERSION}-${ETCD_ARC}.tar.gz
+rm -rf ${ETCD_PATH}/etcd-v${ETCD_VERSION}-${ETCD_ARC}
+rm -rf ${ETCD_PATH}/etcd
+rm -rf ${ETCD_PATH}/default.etcd
+
+if [ ! -d ${ETCD_PATH} ]; then
+    mkdir ${ETCD_PATH}
+fi
+
+# install etcd
+curl -L ${ETCD_URL}/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-${ETCD_ARC}.tar.gz \
+    | tar xzf - \
+        --directory ${ETCD_PATH} \
+        --strip-components=1
+
+# check
+${ETCD_PATH}/etcd --version
