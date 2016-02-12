c3672bf617e69d4b0d794df2f89d3cc7577e4a5b
Christian Trimble <christian.trimble@meltmedia.com>
2016-02-12 02:25:44 -0700

    use provided scripts when setting up vagrant

--- src/integration/vagrant/provision.sh
+++ src/integration/vagrant/provision.sh
@@ -2,8 +2,7 @@
 sudo apt-get update
 
 # install etcd from a github tag.
-sudo mkdir /opt/etcd-v2.2.4-linux-amd64
-curl -Ls https://github.com/coreos/etcd/releases/download/v2.2.4/etcd-v2.2.4-linux-amd64.tar.gz | sudo tar xzv --strip-components=1 -C /opt/etcd-v2.2.4-linux-amd64
+/vagrant/src/main/bash/etcd_install.sh "/opt/etcd-v2.2.4-linux-amd64"
 
 sudo cp /vagrant/src/integration/vagrant/etcd.conf /etc/init
 sudo service etcd start
