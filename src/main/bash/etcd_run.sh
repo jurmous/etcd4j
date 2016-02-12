ee4d95d809b600051ce7fbf0056f0842c7e4b3e2
Christian Trimble <christian.trimble@meltmedia.com>
2016-02-12 02:23:23 -0700

    added argument to pick bind interface run etcd in the foreground

--- src/main/bash/etcd_run.sh
+++ src/main/bash/etcd_run.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
 
+OPTIND=0
+
+while getopts ":b:" opt; do
+  case $opt in
+  	b)
+      BIND_ADDRESS=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit -1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+ETCD_ARGS=( '--name' 'etcd4j.etcd' )
+
 if [ $# -eq 1 ]; then
     ETCD_PATH=$1
-
-    ${ETCD_PATH}/etcd \
-        --name 'etcd4j.etcd' \
-        --data-dir ${ETCD_PATH}/etcd4j.etcd &
+    ETCD_ARGS=(${ETCD_ARGS[@]} '--data-dir' "${ETCD_PATH}/etcd4j.etcd")
 else
     echo "Installation path is missing"
     exit -1
 fi
+
+if [ "${BIND_ADDRESS+x}" ]; then
+	ETCD_ARGS=(${ETCD_ARGS[@]} \
+	    "-listen-client-urls=http://${BIND_ADDRESS}:2379,http://${BIND_ADDRESS}:4001" \
+	    "-advertise-client-urls=http://localhost:2379,http://localhost:4001" )
+fi
+
+${ETCD_PATH}/etcd ${ETCD_ARGS[@]}
